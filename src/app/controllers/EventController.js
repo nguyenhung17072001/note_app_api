c0e65f0c89e171afaad1388c5adb449172c85e34
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-11-10 18:26:41 +0700

    update

--- src/app/controllers/EventController.js
+++ src/app/controllers/EventController.js
@@ -1,4 +1,5 @@
 const Event = require('../models/Event')
+const User = require('../models/User')
 const {mutipleMongooseToObject, mongooseToObject} = require('../../util/mongoose')
 
 
@@ -13,21 +14,61 @@ const fail = {
 };
 
 class EventControllers {
-    // [Get] /event/list/person?id=
+
+    // [Get] /event/list/person
     list(req, res, next) {
         //console.log('req.query: ', req.query)
-        Event.find({userId: req.query.userId})
+        Event.find({userId: req.body.userId})
         .then((event)=> {
             res.json({
                 ...success,
                 data: event,
                 total: event.length,
+
             })
             
         })
         .catch(next)
     }
 
+    // [Post] api/event/insert/position
+    insertPosition(req, res, next) {
+        //res.send('Hung dzai')
+
+        
+        
+        
+        User.find({position: req.body.position})
+        .then((user)=> {
+            //res.json(user)
+            user.forEach(u=> {
+                let formData= {
+                    ...req.body,
+                    name: u.name,
+                    userId: u._id
+                }
+                let event = new Event(formData)
+                //
+                Event.findOne(formData).then((ev)=> {
+                    if(ev){
+                        res.json({
+                            message: "Lịch bị trùng"
+                        })
+                    } else{
+                        event.save()
+                        .then(()=> res.json(success))
+                        
+                    
+                    }
+
+                })
+                
+            })
+            
+        })
+        .catch(next)
+
+    }
 
 
 }
--- src/routes/event.js
+++ src/routes/event.js
@@ -7,7 +7,7 @@ const eventController = require('../app/controllers/EventController')
 
 //search for person by id
 router.get('/list/person', eventController.list)
-
+router.post('/insert/position', eventController.insertPosition)
 
 
 module.exports = router;
